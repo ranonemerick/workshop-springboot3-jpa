43d46d8b4541e6096f332a09255188a649045437
Ranon Emerick <ranonemerick@gmail.com>
2023-04-14 15:03:06 -0300

    h2 databse, teste profile, JPA

--- src/main/java/com/educandoweb/udmone/entities/User.java
+++ src/main/java/com/educandoweb/udmone/entities/User.java
@@ -1,9 +1,12 @@
 package com.educandoweb.udmone.entities;
 
-
-
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -12,12 +15,16 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
+@NoArgsConstructor 
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
+@Table(name = "tb_users")
+@Entity
 public class User implements Serializable  {
-
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
