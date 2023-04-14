7d6c9f0de82c537390b468945f6b71e5f217d07f
Ranon Emerick <ranonemerick@gmail.com>
2023-04-14 09:34:45 -0300

    Project created

---
+++ src/main/java/com/educandoweb/udmone/UdmoneApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.udmone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdmoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdmoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/udmone/UdmoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.udmone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdmoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
