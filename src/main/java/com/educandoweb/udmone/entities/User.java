68ccec04b93e69eae76e93f364113b6548e85a7d
Ranon Emerick <ranonemerick@gmail.com>
2023-04-14 10:48:07 -0300

    user and resource

---
+++ src/main/java/com/educandoweb/udmone/entities/User.java
@@ -0,0 +1,27 @@
+package com.educandoweb.udmone.entities;
+
+
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User implements Serializable  {
+
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
+	
+}
