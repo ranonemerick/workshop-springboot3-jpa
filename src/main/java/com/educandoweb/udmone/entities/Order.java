99f4a010ca2cbfcb4b48f0b3606b75b8d0f9d526
Ranon Emerick <ranonemerick@gmail.com>
2023-04-17 16:40:58 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/educandoweb/udmone/entities/Order.java
+++ src/main/java/com/educandoweb/udmone/entities/Order.java
@@ -1,41 +1,38 @@
 package com.educandoweb.udmone.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Instant;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "tb_order")
 @Getter
 @Setter
-@NoArgsConstructor 
-@AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-@Table(name = "tb_users")
-@Entity
-public class User implements Serializable  {
-	private static final long serialVersionUID = 1L;
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String name;
-	private String email;
-	private String phone; 
-	private String password;
-	@OneToMany(mappedBy = "client")
-	@Setter(AccessLevel.NONE)
-	private List<Order> orders = new ArrayList<>();
+	private Instant moment;
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private User client;
 	
+
 }
